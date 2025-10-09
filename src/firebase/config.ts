5136f969d4bc97284aac9b5afb6ee0b4fe47ef22
Murali karthikeya Repala <geetha9849440923@gmail.com>
2025-10-09 18:33:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9515628946-48f5d",
+  "appId": "1:597524277145:web:1b05a0b91f855908b49508",
+  "apiKey": "AIzaSyCEk3mPXxgIvwMG6EYclR5A5UUJlOBuAqM",
+  "authDomain": "studio-9515628946-48f5d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "597524277145"
+};
